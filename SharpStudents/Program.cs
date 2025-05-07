6f946d8f4d0f07744b5925012aa5c672b6abe7b4
jerecede <jeremias10458@gmail.com>
2025-05-07 13:27:42 +0200

    Add project files.

---
+++ SharpStudents/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SharpStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("jeremias", "cedeno", "male", 2003, new List<int> {5,4,10});
+            Console.WriteLine(s1.Mean());
+            Student s2 = new Student("hugo", "martinez", "male", 1995, new List<int> { 7, 9, 10 });
+            Student s3 = new Student("jan", "stigliani", "male", 200, new List<int> { 7, 8, 6 });
+
+            List<Student> students = [ s1, s2, s3 ];
+
+            students.Aggregate(student => Console.WriteLine(student.mean));
+        }
+    }
+}
---
+++ SharpStudents/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpStudents
+{
+    internal class Student
+    {
+        public string Name { get; set; } //posso decidere se togliere il set
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+
+        private int _yob;
+        public int Yob
+        {
+            get => _yob;
+            set
+            {
+                if (value < 1900)
+                {
+                    _yob = 1900;
+                }
+                else
+                {
+                    _yob = value;
+                }
+            }
+        }
+
+        private List<int> _marks = [];
+        public List<int>? Marks {
+            get => _marks;
+            set => _marks = value;
+        }
+
+        public Student(string name, string surname, string gender, int yob, List<int> marks)
+        {
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+            Yob = yob;
+            Marks = marks;
+        }
+
+        public double Mean()
+        {
+            return (double)this._marks.Sum() / this._marks.Count; //basta che uno sia castato
+        }
+    }
+}
